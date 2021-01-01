f757ffc0dd1f9d6b8903aac287ffc388cb83339c
Jamal Derrick <jamalderrick6@gmail.com>
2021-01-01 13:19:11 +0300

    [react-blog] create graphql backend

---
+++ graphql/schemas/index.js
@@ -0,0 +1,14 @@
+const postSchema = require('./posts')
+
+const resolvers = [
+    postSchema.resolvers
+]
+
+const typeDefs = [
+    postSchema.schema
+]
+
+module.exports = {
+    resolvers,
+    typeDefs
+}
